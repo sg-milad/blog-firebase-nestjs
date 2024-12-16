a3341374bf7e2ae825ef33c15e9feb4c28a990c0
milad <miladbachari14@gmail.com>
2024-12-16 23:14:29 +0330

    feat: add user module to app

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModuleOptions } from './configs/config.module.options';
 import { ConfigModule } from '@nestjs/config';
+import { UserModule } from './user/user.module';
 
 @Module({
-  imports: [ConfigModule.forRoot(ConfigModuleOptions()),],
+  imports: [ConfigModule.forRoot(ConfigModuleOptions()), UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
