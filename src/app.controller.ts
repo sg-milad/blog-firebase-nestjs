0e39f86db154cb4e2844e0a34061a30054106218
milad <miladbachari14@gmail.com>
2024-12-16 23:25:01 +0330

    refactor: controller and module

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,27 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Auth } from './decorators/auth.decorator';
+import { ApiOperation } from '@nestjs/swagger';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
+  @Get('/admin')
+  @Auth('ADMIN')
+  @ApiOperation({ description: "only admin" })
+  onlyAdmin() {
+    return 'hi admin';
+  }
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Get('/dev')
+  @Auth('DEVELOPER')
+  @ApiOperation({ description: "only developer" })
+  onlyDeveloper() {
+    return 'hi devs do you need coffee?';
+  }
+
+  @Get('/public')
+  public() {
+    return 'hi to society';
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,11 @@ import { AppService } from './app.service';
 import { ConfigModuleOptions } from './configs/config.module.options';
 import { ConfigModule } from '@nestjs/config';
 import { UserModule } from './user/user.module';
+import { FirebaseAdmin } from './configs/firebase.setup';
 
 @Module({
   imports: [ConfigModule.forRoot(ConfigModuleOptions()), UserModule],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, FirebaseAdmin],
 })
 export class AppModule { }
