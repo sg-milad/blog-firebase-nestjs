8841b29b10bdb44b526e2506f12fb735943636a8
milad <miladbachari14@gmail.com>
2024-12-16 23:14:51 +0330

    feat: add firebase config

--- src/configs/firebase.setup.ts
+++ src/configs/firebase.setup.ts
@@ -1,15 +1,16 @@
 import { Injectable, OnApplicationBootstrap } from '@nestjs/common';
 import { readFile } from 'fs/promises';
 import * as admin from 'firebase-admin';
+import { resolve } from 'path';
 let app: admin.app.App
+
 @Injectable()
 export class FirebaseAdmin implements OnApplicationBootstrap {
   async onApplicationBootstrap() {
     if (!app) {
-      const firebaseServiceAccountFile = await readFile(
-        '../config/firebaseServiceAccountKey.json',
-        'utf8',
-      );
+      const filePath = resolve(__dirname, 'firebaseServiceAccountKey.json');
+      const firebaseServiceAccountFile = await readFile(filePath, 'utf8');
+
       const serviceAccount = await JSON.parse(firebaseServiceAccountFile);
       app = admin.initializeApp({
         credential: admin.credential.cert(serviceAccount),
--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,10 +1,10 @@
 import { Module } from '@nestjs/common';
-import { FirebaseAdmin } from 'config/firebase.setup';
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
+import { FirebaseAdmin } from 'src/configs/firebase.setup';
 
 @Module({
   controllers: [UserController],
   providers: [UserService, FirebaseAdmin],
 })
-export class UserModule {}
+export class UserModule { }
