a494811468a3a27a459069e1ba08841dee29f015
milad <miladbachari14@gmail.com>
2024-12-16 12:51:48 +0330

    feat: add swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import swagger from './shared/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+
+  app.useGlobalPipes(new ValidationPipe({
+    transform: true,
+    whitelist: true,
+  }));
+
+  swagger.configure(app);
+
   await app.listen(3000);
 }
 bootstrap();
